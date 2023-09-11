7fdf34d55d36920443a4f1de5d7cfeeb8771c6cb
Cassidy <cassidymcmichael617@gmail.com>
2023-09-11 18:23:54 -0400

    change heading and text for second section based on slideIndex value

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const openCloseNav = () => {
     let e = document.getElementById("links");
     let n = document.getElementById("nav-menu");
     let io = document.getElementById("icon-open");
-    let ic= document.getElementById("icon-close");
+    let ic = document.getElementById("icon-close");
     let l = document.getElementById("logo");
 
     if (e.style.display === "inline-block") {
@@ -36,22 +36,54 @@ const showSlides = (n) => {
     let url2 = "./images/mobile-image-hero-2.jpg";
     let url3 = "./images/mobile-image-hero-3.jpg";
     let div = document.getElementById("hero");
+    let h = document.getElementById("heading-text-main");
+    let p = document.getElementById("body-text-main");
 
     //console.log(`value of n is: ${n}`);
 
     switch (n) {
-        case 1: 
+        case 1:
             div.style.backgroundImage = `url(${url1})`;
-        break;
+            h.innerHTML = "Discover innovative ways to decorate";
+            p.innerHTML =
+                `
+                Our multifunctional collection blends design and function to suit your individual taste.
+                Make each room unique, or pick a cohesive theme that best express your interests and what
+                inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+                or anything in between. Product specialists are available to help you create your dream space.
+                `;
+            break;
         case 2:
             div.style.backgroundImage = `url(${url2})`;
-        break;
-        case 3: 
-        div.style.backgroundImage = `url(${url3})`;
-        break;
+            h.innerHTML = "We are available all accross the globe";
+            p.innerHTML =                 
+            `
+            With stores all over the world, it's easy for you to find furniture for your home or place of business. 
+            Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
+            store locator. Any questions? Don't hesitate to contact us today.
+            `;
+            break;
+        case 3:
+            div.style.backgroundImage = `url(${url3})`;
+            h.innerHTML = "Manufactured with the best materials";
+            p.innerHTML = 
+            `
+            Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+            to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+            experience in this industry, we understand what customers want for their home and office.
+            `;
+            break;
         default:
             div.style.backgroundImage = `url(${url1})`;
-            slideIndex  = 1;
-        break;
+            h.innerHTML = "Discover innovative ways to decorate";
+            p.innerHTML = 
+            `
+            Our multifunctional collection blends design and function to suit your individual taste.
+            Make each room unique, or pick a cohesive theme that best express your interests and what
+            inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+            or anything in between. Product specialists are available to help you create your dream space.
+            `;
+            slideIndex = 1;
+            break;
     }
 }
