7fd50ba7d3c454ea1731c0a6a665c961cd39f1e0
Cassidy <cassidymcmichael617@gmail.com>
2023-09-13 10:37:40 -0400

    update grid-template-areas, grid-auto-rows, and grid-template-columns

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const openCloseNav = () => {
-    console.log('HIT');
     let e = document.getElementById("links");
     let n = document.getElementById("nav-menu");
     let io = document.getElementById("icon-open");
@@ -40,7 +39,7 @@ const showSlides = (n) => {
     let vpw = window.innerWidth;
 
     if (vpw <= 899) {
-            switch (n) {
+        switch (n) {
             case 1:
                 div.style.backgroundImage = `url(${mobileURL1})`;
                 h.innerHTML = "Discover innovative ways to decorate";
@@ -55,8 +54,8 @@ const showSlides = (n) => {
             case 2:
                 div.style.backgroundImage = `url(${mobileURL2})`;
                 h.innerHTML = "We are available all accross the globe";
-                p.innerHTML =                 
-                `
+                p.innerHTML =
+                    `
                 With stores all over the world, it's easy for you to find furniture for your home or place of business. 
                 Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
                 store locator. Any questions? Don't hesitate to contact us today.
@@ -65,8 +64,8 @@ const showSlides = (n) => {
             case 3:
                 div.style.backgroundImage = `url(${mobileURL3})`;
                 h.innerHTML = "Manufactured with the best materials";
-                p.innerHTML = 
-                `
+                p.innerHTML =
+                    `
                 Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
                 to ensure that every product is made as perfect and as consistent as possible. With three decades of 
                 experience in this industry, we understand what customers want for their home and office.
@@ -75,8 +74,8 @@ const showSlides = (n) => {
             default:
                 div.style.backgroundImage = `url(${mobileURL1})`;
                 h.innerHTML = "Discover innovative ways to decorate";
-                p.innerHTML = 
-                `
+                p.innerHTML =
+                    `
                 Our multifunctional collection blends design and function to suit your individual taste.
                 Make each room unique, or pick a cohesive theme that best express your interests and what
                 inspires you. Find the furniture pieces you need, from traditional to contemporary styles
@@ -100,8 +99,8 @@ const showSlides = (n) => {
             case 2:
                 div.style.backgroundImage = `url(${desktopURL2})`;
                 h.innerHTML = "We are available all accross the globe";
-                p.innerHTML =                 
-                `
+                p.innerHTML =
+                    `
                 With stores all over the world, it's easy for you to find furniture for your home or place of business. 
                 Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
                 store locator. Any questions? Don't hesitate to contact us today.
@@ -110,8 +109,8 @@ const showSlides = (n) => {
             case 3:
                 div.style.backgroundImage = `url(${desktopURL3})`;
                 h.innerHTML = "Manufactured with the best materials";
-                p.innerHTML = 
-                `
+                p.innerHTML =
+                    `
                 Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
                 to ensure that every product is made as perfect and as consistent as possible. With three decades of 
                 experience in this industry, we understand what customers want for their home and office.
@@ -120,8 +119,8 @@ const showSlides = (n) => {
             default:
                 div.style.backgroundImage = `url(${desktopURL1})`;
                 h.innerHTML = "Discover innovative ways to decorate";
-                p.innerHTML = 
-                `
+                p.innerHTML =
+                    `
                 We provide unmatched quality, comfort, and style for property owners across the country. 
                 Our experts combine form and function in bringing your vision to life. Create a room in your 
                 own style with our collection and make your property a reflection of you and what you love.
@@ -130,6 +129,4 @@ const showSlides = (n) => {
                 break;
         }
     }
-
-   
 }
