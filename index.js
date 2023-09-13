8a6a4b1bcaa620ecd6f68fe7ef8403d981e98519
Cassidy <cassidymcmichael617@gmail.com>
2023-09-12 22:03:26 -0400

    replace text for slideIndex 1 and fix desktop layout

--- index.js
+++ index.js
@@ -105,10 +105,9 @@ const showSlides = (n) => {
                 h.innerHTML = "Discover innovative ways to decorate";
                 p.innerHTML =
                     `
-                    Our multifunctional collection blends design and function to suit your individual taste.
-                    Make each room unique, or pick a cohesive theme that best express your interests and what
-                    inspires you. Find the furniture pieces you need, from traditional to contemporary styles
-                    or anything in between. Product specialists are available to help you create your dream space.
+                    We provide unmatched quality, comfort, and style for property owners across the country. 
+                    Our experts combine form and function in bringing your vision to life. Create a room in your 
+                    own style with our collection and make your property a reflection of you and what you love.
                     `;
                 break;
             case 2:
@@ -136,10 +135,9 @@ const showSlides = (n) => {
                 h.innerHTML = "Discover innovative ways to decorate";
                 p.innerHTML = 
                 `
-                Our multifunctional collection blends design and function to suit your individual taste.
-                Make each room unique, or pick a cohesive theme that best express your interests and what
-                inspires you. Find the furniture pieces you need, from traditional to contemporary styles
-                or anything in between. Product specialists are available to help you create your dream space.
+                We provide unmatched quality, comfort, and style for property owners across the country. 
+                Our experts combine form and function in bringing your vision to life. Create a room in your 
+                own style with our collection and make your property a reflection of you and what you love.
                 `;
                 slideIndex = 1;
                 break;
